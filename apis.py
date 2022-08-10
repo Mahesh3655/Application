475cf6d03017dc9487bbdfdb12e4a33ae3089a31
Mahesh3655 <saimaheshvasarla@gmail.com>
2022-08-10 12:04:40 +0530

    Get updated

--- apis.py
+++ apis.py
@@ -1,9 +1,9 @@
-import flask
+from flask import Flask,make_response,abort
 import json
 
 from pymongo import MongoClient
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
 
 @app.route("/",methods=['GET'])
 def gcff():
@@ -14,6 +14,28 @@ def gcff():
     for i in e.find():
         lst.append(i)
     return json.dumps(lst)
-        
+
+@app.route("/getid/<int:id>",methods=['GET'])
+def get_emp_id(id):
+    client = MongoClient()
+    db = client.company
+    e = db.Employees
+    lst =[]
+    for i in e.find():
+        lst.append(i)
+    employee = []
+
+    for emp in lst:
+        if emp["_id"] == id:
+            employee.append(emp)
+    if len(employee) == 0:
+        abort(404)
+    return json.dumps(employee)
+
+@app.errorhandler(404)
+def notfound(error):
+    return make_response("Not Found",404)
+
+
 
 app.run(port=5050)
